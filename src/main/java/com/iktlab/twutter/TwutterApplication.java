adc93835d17983bfd886fe952c4899bd1e65620f
Aveira <kirito.html@gmail.com>
2023-11-06 15:46:09 +0400

    root folder has been created

---
+++ src/main/java/com/iktlab/twutter/TwutterApplication.java
@@ -0,0 +1,13 @@
+package com.iktlab.twutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwutterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwutterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iktlab/twutter/TwutterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iktlab.twutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwutterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
